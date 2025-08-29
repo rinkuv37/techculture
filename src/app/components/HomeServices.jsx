f569074be90e80f42825699c37ef033755a9a081
rinkuv37 <rinkuv37@gmail.com>
2025-08-29 18:24:00 +0530

    updated

--- src/app/components/HomeServices.jsx
+++ src/app/components/HomeServices.jsx
@@ -1,201 +1,64 @@
+import Button from '@mui/material/Button'
+import Link from 'next/link'
 import React from 'react'
 import { IoIosArrowRoundForward } from "react-icons/io";
 
 const HomeServices = () => {
     return (
-        <section className='py-20 bg-white'>
+        <section className='py-20 bg-gray-800'>
             <div className='container'>
-                <div className='flex items-center justify-center'>
-                    <span className='border border-[rgba(0,0,0,0.1)] rounded-full px-3 py-1 text-gred font-bold'>Our Service</span>
-                </div>
+                <h2 className='text-white text-[20px]'>SERVICES</h2>
+                <p className='text-white font-light text-[35px] py-3'>Transform your business with advanced technologies</p>
 
 
-                <h2 className='text-center text-[40px] font-bold'>Comprehensive <span className='text-gred'>Solutions</span></h2>
+                <div className='grid grid-cols-4 gap-10 py-4 services'>
+                    <Link href={"#"} className='box rounded-md overflow-hidden relative group'>
+                        <img src={"https://www.tcs.com/content/dam/global-tcs/en/images/what-we-do/ai-insights-card-2.png/jcr:content/renditions/cq5dam.thumbnail.640.640.png"} className='full transition-all group-hover:scale-105 h-full object-cover' alt='image' />
+                        <div className='info p-6 absolute top-0 left-0 z-50 w-full h-full '>
+                           <div className='flex justify-between items-center !absolute bottom-5 pr-5 w-[96%]'>
+                                <h2 className='text-gray-100 text-[25px] font-light'>AI and Data & Analytics</h2>
+                                <Button className='!w-[40px] !h-[40px] !min-w-[40px] !rounded-full !bg-gradient-to-r from-[#ff6333] via-[#e15226] to-[#fe9272] '><IoIosArrowRoundForward className='-rotate-45 text-white' size={35} /></Button>
 
-                <p className='text-gray-700 my-1 text-center text-[18px]'>Transform your business with our comprehensive technology solutions designed for sustainable growth</p>
-
-                <br /><br />
-
-                <div className='grid grid-cols-3 gap-10'>
-                    <div className='box bg-gray-50 shadow-md rounded-md p-6 px-8 py-8 flex flex-col gap-4 group serviceBox'>
-                        <div className='flex items-center justify-between mb-4 serviceImg'>
-                            <div className='img relative'>
-                                <img src={"/service__6.png"} alt='image' className='relative z-50' />
-                                <div className="service__bg__img absolute -top-1 -left-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="165" height="150" viewBox="0 0 165 150" fill="none">
-                                        <path d="M165 65C165 106.421 116.421 150 75 150C33.5786 150 0 116.421 0 75C0 33.5786 33.5786 0 75 0C116.421 0 165 23.5786 165 65Z" fill="url(#paint0_linear_1212_422)"></path>
-                                        <defs>
-                                            <linearGradient id="paint0_linear_1212_422" x1="3.36937e-07" y1="87.931" x2="165" y2="87.931" gradientUnits="userSpaceOnUse">
-                                                <stop offset="0.479167" stop-color="#00229E"></stop>
-                                                <stop offset="0.479167" stop-color="#6E1299"></stop>
-                                                <stop offset="1" stop-color="#FE0094"></stop>
-                                            </linearGradient>
-                                        </defs>
-                                    </svg>
-                                </div>
                             </div>
-
-                            <button className='flex items-center justify-center w-12 h-12 bg-gradient-to-r from-[#ff6333] via-[#e15226] to-[#fe9272] rounded-full -rotate-45'>
-                                <IoIosArrowRoundForward size={30} className='text-white' />
-                            </button>
                         </div>
+                    </Link>
 
-                        <div className='serviceContent'>
-                            <h2 className='text-[22px] font-bold text-gray-800'>AGENCY CONSULTING</h2>
-                            <p className='text-[16px] text-gray-600 leading-7'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since</p>
-                        </div>
-                    </div>
+                    <Link href={"#"} className='box rounded-md overflow-hidden relative group'>
+                        <img src={"https://www.tcs.com/content/dam/global-tcs/ja/images/insights/trends/cloud-business-case-model-transformation/card.jpg/jcr:content/renditions/cq5dam.thumbnail.640.640.jpeg"} className='full transition-all group-hover:scale-105 h-full object-cover' alt='image' />
+                        <div className='info p-6 absolute top-0 left-0 z-50 w-full h-full '>
+                             <div className='flex justify-between items-center !absolute bottom-5 pr-5 w-[96%]'>
+                                <h2 className='text-gray-100 text-[25px] font-light'>Cloud</h2>
+                                <Button className='!w-[40px] !h-[40px] !min-w-[40px] !rounded-full !bg-gradient-to-r from-[#ff6333] via-[#e15226] to-[#fe9272] '><IoIosArrowRoundForward className='-rotate-45 text-white' size={35} /></Button>
 
-
-                     <div className='box bg-gray-50 shadow-md rounded-md p-6 px-8 py-8 flex flex-col gap-4 group serviceBox'>
-                        <div className='flex items-center justify-between mb-4 serviceImg'>
-                            <div className='img relative'>
-                                <img src={"/service__7.png"} alt='image' className='relative z-50' />
-                                <div className="service__bg__img absolute -top-1 -left-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="165" height="150" viewBox="0 0 165 150" fill="none">
-                                        <path d="M165 65C165 106.421 116.421 150 75 150C33.5786 150 0 116.421 0 75C0 33.5786 33.5786 0 75 0C116.421 0 165 23.5786 165 65Z" fill="url(#paint0_linear_1212_422)"></path>
-                                        <defs>
-                                            <linearGradient id="paint0_linear_1212_422" x1="3.36937e-07" y1="87.931" x2="165" y2="87.931" gradientUnits="userSpaceOnUse">
-                                                <stop offset="0.479167" stop-color="#00229E"></stop>
-                                                <stop offset="0.479167" stop-color="#6E1299"></stop>
-                                                <stop offset="1" stop-color="#FE0094"></stop>
-                                            </linearGradient>
-                                        </defs>
-                                    </svg>
-                                </div>
                             </div>
-
-                            <button className='flex items-center justify-center w-12 h-12 bg-gradient-to-r from-[#ff6333] via-[#e15226] to-[#fe9272] rounded-full -rotate-45'>
-                                <IoIosArrowRoundForward size={30} className='text-white' />
-                            </button>
                         </div>
-
-                        <div className='serviceContent'>
-                            <h2 className='text-[22px] font-bold text-gray-800'>IT CONSULTING</h2>
-                            <p className='text-[16px] text-gray-600 leading-7'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since</p>
-                        </div>
-                    </div>
+                    </Link>
 
 
-                     <div className='box bg-gray-50 shadow-md rounded-md p-6 px-8 py-8 flex flex-col gap-4 group serviceBox'>
-                        <div className='flex items-center justify-between mb-4 serviceImg'>
-                            <div className='img relative'>
-                                <img src={"/service__6.png"} alt='image' className='relative z-50' />
-                                <div className="service__bg__img absolute -top-1 -left-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="165" height="150" viewBox="0 0 165 150" fill="none">
-                                        <path d="M165 65C165 106.421 116.421 150 75 150C33.5786 150 0 116.421 0 75C0 33.5786 33.5786 0 75 0C116.421 0 165 23.5786 165 65Z" fill="url(#paint0_linear_1212_422)"></path>
-                                        <defs>
-                                            <linearGradient id="paint0_linear_1212_422" x1="3.36937e-07" y1="87.931" x2="165" y2="87.931" gradientUnits="userSpaceOnUse">
-                                                <stop offset="0.479167" stop-color="#00229E"></stop>
-                                                <stop offset="0.479167" stop-color="#6E1299"></stop>
-                                                <stop offset="1" stop-color="#FE0094"></stop>
-                                            </linearGradient>
-                                        </defs>
-                                    </svg>
-                                </div>
+                    <Link href={"#"} className='box rounded-md overflow-hidden relative group'>
+                        <img src={"https://www.tcs.com/content/dam/global-tcs/en/images/who-we-are/inverstor-relations-poster.png/jcr:content/renditions/cq5dam.thumbnail.640.640.png"} className='full transition-all group-hover:scale-105 h-full object-cover' alt='image' />
+                        <div className='info p-6 absolute top-0 left-0 z-50 w-full h-full '>
+                             <div className='flex justify-between items-center !absolute bottom-5 pr-5 w-[96%]'>
+                                <h2 className='text-gray-100 text-[25px] font-light'>Cognitive Business Operations</h2>
+                                <Button className='!w-[40px] !h-[40px] !min-w-[40px] !rounded-full !bg-gradient-to-r from-[#ff6333] via-[#e15226] to-[#fe9272] '><IoIosArrowRoundForward className='-rotate-45 text-white' size={35} /></Button>
+
                             </div>
-
-                            <button className='flex items-center justify-center w-12 h-12 bg-gradient-to-r from-[#ff6333] via-[#e15226] to-[#fe9272] rounded-full -rotate-45'>
-                                <IoIosArrowRoundForward size={30} className='text-white' />
-                            </button>
                         </div>
+                    </Link>
 
-                        <div className='serviceContent'>
-                            <h2 className='text-[22px] font-bold text-gray-800'>LEGAL CONSULTING</h2>
-                            <p className='text-[16px] text-gray-600 leading-7'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since</p>
-                        </div>
-                    </div>
 
-<div className='box bg-gray-50 shadow-md rounded-md p-6 px-8 py-8 flex flex-col gap-4 group serviceBox'>
-                        <div className='flex items-center justify-between mb-4 serviceImg'>
-                            <div className='img relative'>
-                                <img src={"/service__6.png"} alt='image' className='relative z-50' />
-                                <div className="service__bg__img absolute -top-1 -left-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="165" height="150" viewBox="0 0 165 150" fill="none">
-                                        <path d="M165 65C165 106.421 116.421 150 75 150C33.5786 150 0 116.421 0 75C0 33.5786 33.5786 0 75 0C116.421 0 165 23.5786 165 65Z" fill="url(#paint0_linear_1212_422)"></path>
-                                        <defs>
-                                            <linearGradient id="paint0_linear_1212_422" x1="3.36937e-07" y1="87.931" x2="165" y2="87.931" gradientUnits="userSpaceOnUse">
-                                                <stop offset="0.479167" stop-color="#00229E"></stop>
-                                                <stop offset="0.479167" stop-color="#6E1299"></stop>
-                                                <stop offset="1" stop-color="#FE0094"></stop>
-                                            </linearGradient>
-                                        </defs>
-                                    </svg>
-                                </div>
+                    <Link href={"#"} className='box rounded-md overflow-hidden relative group'>
+                        <img src={"https://www.tcs.com/content/dam/global-tcs/en/images/what-we-do/industries/travel/case-study/maersk-cloud-iot-platform-automate-reefer-monitoring-system-u-card.jpg/jcr:content/renditions/cq5dam.thumbnail.640.640.jpeg"} className='full transition-all group-hover:scale-105 h-full object-cover' alt='image' />
+                        <div className='info p-6 absolute top-0 left-0 z-50 w-full h-full !pr-5'>
+                            <div className='flex justify-between items-center !absolute bottom-5 pr-5 w-[96%]'>
+                                <h2 className='text-gray-100 text-[25px] font-light'>Consulting</h2>
+                                <Button className='!w-[40px] !h-[40px] !min-w-[40px] !rounded-full !bg-gradient-to-r from-[#ff6333] via-[#e15226] to-[#fe9272] '><IoIosArrowRoundForward className='-rotate-45 text-white' size={35} /></Button>
+
                             </div>
-
-                            <button className='flex items-center justify-center w-12 h-12 bg-gradient-to-r from-[#ff6333] via-[#e15226] to-[#fe9272] rounded-full -rotate-45'>
-                                <IoIosArrowRoundForward size={30} className='text-white' />
-                            </button>
                         </div>
-
-                        <div className='serviceContent'>
-                            <h2 className='text-[22px] font-bold text-gray-800'>AGENCY CONSULTING</h2>
-                            <p className='text-[16px] text-gray-600 leading-7'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since</p>
-                        </div>
-                    </div>
+                    </Link>
 
 
-                     <div className='box bg-gray-50 shadow-md rounded-md p-6 px-8 py-8 flex flex-col gap-4 group serviceBox'>
-                        <div className='flex items-center justify-between mb-4 serviceImg'>
-                            <div className='img relative'>
-                                <img src={"/service__7.png"} alt='image' className='relative z-50' />
-                                <div className="service__bg__img absolute -top-1 -left-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="165" height="150" viewBox="0 0 165 150" fill="none">
-                                        <path d="M165 65C165 106.421 116.421 150 75 150C33.5786 150 0 116.421 0 75C0 33.5786 33.5786 0 75 0C116.421 0 165 23.5786 165 65Z" fill="url(#paint0_linear_1212_422)"></path>
-                                        <defs>
-                                            <linearGradient id="paint0_linear_1212_422" x1="3.36937e-07" y1="87.931" x2="165" y2="87.931" gradientUnits="userSpaceOnUse">
-                                                <stop offset="0.479167" stop-color="#00229E"></stop>
-                                                <stop offset="0.479167" stop-color="#6E1299"></stop>
-                                                <stop offset="1" stop-color="#FE0094"></stop>
-                                            </linearGradient>
-                                        </defs>
-                                    </svg>
-                                </div>
-                            </div>
-
-                            <button className='flex items-center justify-center w-12 h-12 bg-gradient-to-r from-[#ff6333] via-[#e15226] to-[#fe9272] rounded-full -rotate-45'>
-                                <IoIosArrowRoundForward size={30} className='text-white' />
-                            </button>
-                        </div>
-
-                        <div className='serviceContent'>
-                            <h2 className='text-[22px] font-bold text-gray-800'>IT CONSULTING</h2>
-                            <p className='text-[16px] text-gray-600 leading-7'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since</p>
-                        </div>
-                    </div>
-
-
-                     <div className='box bg-gray-50 shadow-md rounded-md p-6 px-8 py-8 flex flex-col gap-4 group serviceBox'>
-                        <div className='flex items-center justify-between mb-4 serviceImg'>
-                            <div className='img relative'>
-                                <img src={"/service__6.png"} alt='image' className='relative z-50' />
-                                <div className="service__bg__img absolute -top-1 -left-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="165" height="150" viewBox="0 0 165 150" fill="none">
-                                        <path d="M165 65C165 106.421 116.421 150 75 150C33.5786 150 0 116.421 0 75C0 33.5786 33.5786 0 75 0C116.421 0 165 23.5786 165 65Z" fill="url(#paint0_linear_1212_422)"></path>
-                                        <defs>
-                                            <linearGradient id="paint0_linear_1212_422" x1="3.36937e-07" y1="87.931" x2="165" y2="87.931" gradientUnits="userSpaceOnUse">
-                                                <stop offset="0.479167" stop-color="#00229E"></stop>
-                                                <stop offset="0.479167" stop-color="#6E1299"></stop>
-                                                <stop offset="1" stop-color="#FE0094"></stop>
-                                            </linearGradient>
-                                        </defs>
-                                    </svg>
-                                </div>
-                            </div>
-
-                            <button className='flex items-center justify-center w-12 h-12 bg-gradient-to-r from-[#ff6333] via-[#e15226] to-[#fe9272] rounded-full -rotate-45'>
-                                <IoIosArrowRoundForward size={30} className='text-white' />
-                            </button>
-                        </div>
-
-                        <div className='serviceContent'>
-                            <h2 className='text-[22px] font-bold text-gray-800'>LEGAL CONSULTING</h2>
-                            <p className='text-[16px] text-gray-600 leading-7'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since</p>
-                        </div>
-                    </div>
-
-                    
                 </div>
 
 
